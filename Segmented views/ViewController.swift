3d35fb25d6f5e4d04bd9b05481200c92f9156370
Wissa Azmy <wissaaymz@wissas-macbook-pro.local>
2018-07-08 14:27:53 +0200

    Initial Commit

---
+++ Segmented views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segmented views
+//
+//  Created by Wissa Azmy on 7/8/18.
+//  Copyright © 2018 Wissa Azmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
