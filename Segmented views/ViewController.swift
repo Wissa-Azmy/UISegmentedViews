cf30f564e80428d6831c68393f345f28162d7b31
Wissa Azmy <wissaaymz@Wissas-MacBook-Pro.local>
2018-09-04 16:48:42 +0200

    Final code

--- Segmented views/ViewController.swift
+++ Segmented views/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func switchViews(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            firstView.alpha = 1
+            secondView.alpha = 0
+        } else {
+            firstView.alpha = 0
+            secondView.alpha = 1
+        }
+    }
 
 }
 
